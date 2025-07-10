2efb856e5d7dbc2ef22777f24c709f2ffc4cec95
VinceBmmrt <vincebommert75@gmail.com>
2025-07-10 17:38:10 +0200

    Bean OutPut Converter With List

--- src/main/java/com/telusko/SpringAIRedisVectorStore/controller/MovieController.java
+++ src/main/java/com/telusko/SpringAIRedisVectorStore/controller/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,17 +24,13 @@ public class MovieController {
     @GetMapping("movies")
     public List<String> getMovies(@RequestParam String name) {
 
-
         List<String> movies = chatClient.prompt()
                 .user(u -> u.text("List top 5 movies of {name}").param("name", name))
                 .call()
                 .entity(new ListOutputConverter(new DefaultConversionService()));
 
-
         return movies;
-
     }
-
 
     @GetMapping("/movie")
     public Movie getMovieData(@RequestParam String name) {
@@ -46,5 +43,23 @@ public class MovieController {
                 .entity(new BeanOutputConverter<Movie>(Movie.class));
 
         return movie;
+    }
+
+    @GetMapping("/moviesList")
+    public List<Movie> getMovieList(@RequestParam String name) {
+
+        BeanOutputConverter<List<Movie>> opCon = new BeanOutputConverter<List<Movie>>(
+                new ParameterizedTypeReference<List<Movie>>() {
+                });
+
+        List<Movie> movies = chatClient.prompt()
+                .user(u -> u.text("Top 5 movies of {name}").param("name", name))
+                .call()
+                .entity(new BeanOutputConverter<List<Movie>>(new ParameterizedTypeReference<List<Movie>>() {
+                }));
+
+        return movies;
+
+
     }
 }
